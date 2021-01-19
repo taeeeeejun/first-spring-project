e0b7e130cdb1fa63549815dddf5968d05fa9d87d
TaeJun Park <kr601435@amway.com>
2021-01-19 17:46:46 +0900

    게시글 보기

--- src/main/java/com/taejun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/taejun/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.taejun.book.springboot.service.posts;
 
 import com.taejun.book.springboot.domain.posts.Posts;
 import com.taejun.book.springboot.domain.posts.PostsRepository;
+import com.taejun.book.springboot.web.dto.PostsListResponseDto;
 import com.taejun.book.springboot.web.dto.PostsResponseDto;
 import com.taejun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.taejun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +38,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
